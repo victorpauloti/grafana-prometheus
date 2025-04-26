7589ef1478a4e79288574e85d1022b90ecbadf5e
Victor Paulo <victorpaulo.ti@gmail.com>
2025-04-26 18:53:31 -0300

    first commit

---
+++ app/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#java -jar -Xms128M -Xmx128M -XX:PermSize=64m -XX:MaxPermSize=128m -Dspring.profiles.active=prod target/forum.jar
+java -jar -Xms128M -Xmx128M -Dspring.profiles.active=prod target/forum.jar
