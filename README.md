8b6f85fff197bd9b7bcb9068808760643882716d
Victor Paulo <victorpaulo.ti@gmail.com>
2025-04-27 20:40:37 -0300

    docker funcional

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # grafana-prometheus
+     - ./prometheus.yml:/etc/prometheus/prometheus.yml
+      - ./prometheus_data:/prometheus
+
+
+
+
+      - '--config.file=/etc/prometheus/prometheus.yml'
+      - '--storage.tsdb.path=/prometheus'
+      - '--web.console.libraries=/etc/prometheus/console_libraries'
+      - '--web.console.templates=/etc/prometheus/consoles'
+      - '--web.enable-lifecycle'
+
+
+      #ENTRYPOINT ["java -jar -Xms128M -Xmx128M -Dspring.profiles.active=prod target/forum.jar"]
+
+      java -jar -Xms128M -Xmx128M -Dspring.profiles.active=prod target/forum.jar"
--- app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -1,7 +1,6 @@
 package br.com.alura.forum.controller;
 
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.controller.dto.TokenDto;
 import br.com.alura.forum.controller.form.LoginForm;
@@ -29,15 +27,15 @@ public class AutenticacaoController {
 	Counter authUserErrors;
 
 	public AutenticacaoController(MeterRegistry registry) {
-    authUserSuccess = Counter.builder("auth_user_success")
-        .description("usuarios autenticados")
-        .register(registry);
-        
-    authUserErrors = Counter.builder("auth_user_error")
-        .description("erros de login")
-        .register(registry);
-	}
-
+    	authUserSuccess = Counter.builder("auth_user_success")
+            .description("usuarios autenticados")
+            .register(registry);
+    	
+    	authUserErrors = Counter.builder("auth_user_error")
+                .description("erros de login")
+                .register(registry);
+    }
+    
 	@Autowired
 	private AuthenticationManager authManager;
 	
@@ -51,14 +49,12 @@ public class AutenticacaoController {
 		try {
 			Authentication authentication = authManager.authenticate(dadosLogin);
 			String token = tokenService.gerarToken(authentication);
-			authUserSuccess.increment();	
+			authUserSuccess.increment();
 			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
 			
 		} catch (AuthenticationException e) {
 			authUserErrors.increment();
 			return ResponseEntity.badRequest().build();
-		}
-
-		
+		}	
 	}
 }
